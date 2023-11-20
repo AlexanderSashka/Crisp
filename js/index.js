b4bc98066d86d49e1b120a05486da93144459b3b
AlexanderSashka <odnorobalex@gmail.com>
2023-11-20 14:21:39 +0200

    add new style

--- js/index.js
+++ js/index.js
@@ -32,3 +32,21 @@ document.body.addEventListener('click', (e) => {
 });
 
 //========================================================================================================================================================
+const parent_original = document.querySelector('.header__actions'),
+	parent = document.querySelector('.menu__body'),
+	item = document.querySelector('.actions-header__auth');
+
+window.addEventListener('resize', function (event) {
+	const viewport_width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+	if (viewport_width < 767.98) {
+		if (!item.classList.contains('done')) {
+			parent.insertBefore(item, parent.children[2]);
+			item.classList.add('done');
+		}
+	} else {
+		if (item.classList.contains('done')) {
+			parent_original.insertBefore(item, parent_original.children[2]);
+			item.classList.remove('done');
+		}
+	}
+	})
