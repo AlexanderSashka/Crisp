e166806c3d6bed5ecf9ffcea59d876f5368979c9
AlexanderSashka <odnorobalex@gmail.com>
2023-12-04 21:14:11 +0200

    text

--- js/create-account.js
+++ js/create-account.js
@@ -3,7 +3,6 @@ window.addEventListener('DOMContentLoaded', () => {
 	const confirmPassword = document.getElementById('confirm');
 	const warningPass = document.querySelector('.warning-pass');
 	const createForm = document.querySelector('.customer__form');
-	const strongPass = document.querySelector('.strong-pass');
 	const formRow = document.querySelector('.form-account__row_pass');
 	
 	createForm.addEventListener('submit', (e) => {
@@ -15,21 +14,27 @@ window.addEventListener('DOMContentLoaded', () => {
 			  warningPass.style.visibility = 'visible';
 			 warningPass.style.transform = 'translate(0,0)';
 			 formRow.style.marginBottom = '35px';
+			 warningPass.classList.remove('strong-pass');
+			 return;
 		 } else if (signInPassword.value.length < 8 || confirmPassword.value.length < 8) {
 			  warningPass.textContent = 'Password Too Short! (min 8 el)';
 			  warningPass.style.opacity = '1';
 			  warningPass.style.visibility = 'visible';
 			 warningPass.style.transform = 'translate(0,0)';
 			 formRow.style.marginBottom = '35px';
+			 warningPass.classList.remove('strong-pass');
+			 return;
 		 } else {
-			strongPass.textContent = 'Strong Password!';
-			strongPass.style.opacity = '1';
-			strongPass.style.visibility = 'visible';
-			 strongPass.style.transform = 'translate(0,0)';
-			 warningPass.style.opacity = '0';
-			 warningPass.style.visibility = 'hidden';
+			 warningPass.textContent = 'Strong Password!';
+			 warningPass.classList.add('strong-pass');
+			//  warningPass.style.backgroung = '#008000';
+			// strongPass.style.opacity = '1';
+			// strongPass.style.visibility = 'visible';
+			//  strongPass.style.transform = 'translate(0,0)';
+			//  warningPass.style.opacity = '0';
+			//  warningPass.style.visibility = 'hidden';
 			 formRow.style.marginBottom = '35px';
-			 warningPass.style.display = 'none';
+		
 			
 		 }
 	});
