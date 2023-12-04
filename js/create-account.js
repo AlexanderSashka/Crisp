9603b45ccbcb6b61e32d8cf1fa6cde2573426d66
AlexanderSashka <odnorobalex@gmail.com>
2023-12-04 21:37:37 +0200

    text

--- js/create-account.js
+++ js/create-account.js
@@ -8,37 +8,31 @@ window.addEventListener('DOMContentLoaded', () => {
 	createForm.addEventListener('submit', (e) => {
 		 e.preventDefault();
 	
-		 if (signInPassword.value !== confirmPassword.value) {
-			  warningPass.textContent = 'Passwords do not match';
-			  warningPass.style.opacity = '1';
-			  warningPass.style.visibility = 'visible';
-			 warningPass.style.transform = 'translate(0,0)';
+		 if (signInPassword.value.length < 8 && confirmPassword.value.length < 8) {
+			warningPass.textContent = 'Password Too Short! (min 8 el)';
+			warningPass.style.opacity = '1';
+			warningPass.style.visibility = 'visible';
+		 	warningPass.style.transform = 'translate(0,0)';
+		 	formRow.style.marginBottom = '35px';
+		 	warningPass.classList.remove('strong-pass');
+		  return;
+		 }else {
+			warningPass.textContent = 'Passwords do not match';
 			 formRow.style.marginBottom = '35px';
-			 warningPass.classList.remove('strong-pass');
-			 return;
-		 } else if (signInPassword.value.length < 8 || confirmPassword.value.length < 8) {
-			  warningPass.textContent = 'Password Too Short! (min 8 el)';
-			  warningPass.style.opacity = '1';
-			  warningPass.style.visibility = 'visible';
-			 warningPass.style.transform = 'translate(0,0)';
-			 formRow.style.marginBottom = '35px';
-			 warningPass.classList.remove('strong-pass');
-			 return;
-		 } else {
-			 warningPass.textContent = 'Strong Password!';
-			 warningPass.classList.add('strong-pass');
-			//  warningPass.style.backgroung = '#008000';
-			// strongPass.style.opacity = '1';
-			// strongPass.style.visibility = 'visible';
-			//  strongPass.style.transform = 'translate(0,0)';
-			//  warningPass.style.opacity = '0';
-			//  warningPass.style.visibility = 'hidden';
-			 formRow.style.marginBottom = '35px';
-		
-			
-		 }
-	});
-	
-	
+		}
 
+		if (signInPassword.value !== confirmPassword.value) {
+			warningPass.textContent = 'Passwords do not match';
+					warningPass.style.opacity = '1';
+					warningPass.style.visibility = 'visible';
+				  warningPass.style.transform = 'translate(0,0)';
+				  formRow.style.marginBottom = '35px';
+				  warningPass.classList.remove('strong-pass');
+				  return;
+		 }else {
+				 warningPass.textContent = 'Strong Password!';
+				 warningPass.classList.add('strong-pass');
+				 formRow.style.marginBottom = '35px';
+			 }
+	});
 });
