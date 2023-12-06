b54855872261f0fe29884e07938e78e5c3c7a24e
AlexanderSashka <odnorobalex@gmail.com>
2023-12-06 16:36:40 +0200

    text

--- js/index.js
+++ js/index.js
@@ -193,4 +193,13 @@ const signInPopupClose = document.querySelector('.form-sign-in__close-button');
 	}
 	})();
 	//========================================================================================================================================================
+	const slideValueMin = document.querySelector('.price-filter__value');
+	const inputSlider = document.querySelector('.price-filter__input');
+	
+	inputSlider.oninput = (() => {
+		 let value = inputSlider.value;
+		 slideValueMin.textContent = `${value} .00 EUR`;
+		 slideValueMin.style.left = (value / 2) + '%';
+	});
+	
 });
